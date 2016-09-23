4068dd8a80787a985d912ef8ae44ab293481ecc5
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2016-09-23 10:42:41 +0000

    configure arch scripts

---
+++ archlinux/scripts/zsh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+echo "Installing zsh and my zshrc"
+sudo pacman -S --noconfirm zsh
+cd ../../
+make install-zsh
+
+echo "configuring zsh as default shell"
+chsh -s /bin/zsh $USER
