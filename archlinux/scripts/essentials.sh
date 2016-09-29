3c6680364ebf6226be09ff948f7e726c2724140b
Leonardo Cesar Borges <leocborgess@gmail.com>
2016-09-29 08:43:25 -0300

    update essentials

--- archlinux/scripts/essentials.sh
+++ archlinux/scripts/essentials.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-pacman -S xorg-server-devel
-pacman -S nvidia
-pacman -S nvidia-settings
+
+echo "nvidia stuffs"
+pacman --noconfirm -S xorg-server-devel nvidia nvidia-settings
+
+sudo pacman --noconfirm -S git openssh curl dmenu xclip unrar slock numlockx wget
+
+git config --global push.default simple
+git config --global user.name "Leonardo Cesar Borges"
+git config --global user.email "leocborgess@gmail.com"
+
+echo "Configure time properly"
+sudo ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+sudo timedatectl set-ntp true
+
+echo "Configure keyboard layout"
+sudo localectl set-keymap br-abnt2
